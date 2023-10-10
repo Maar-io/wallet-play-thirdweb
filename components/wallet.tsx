da4a9b4c0f4a7210647b33dedc67e2a319649827
Mar.io <34627453+Maar-io@users.noreply.github.com>
2023-10-10 14:57:24 +0200

    branding

--- components/wallet.tsx
+++ components/wallet.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/Home.module.css";
 import {
     ConnectWallet,
 } from "@thirdweb-dev/react";
-
+import { TOKEN_CONTRACT_ADDRESS } from "../const/addresses";
 
 export default function Wallet() {
 
@@ -10,17 +10,30 @@ export default function Wallet() {
         <div className={styles.connect}>
             <ConnectWallet
                 theme={"light"}
-                btnTitle={"Connect"}
+                btnTitle={"zkAstar Connect"}
+                supportedTokens={{
+                    [80001]: [
+                        {
+                    address: TOKEN_CONTRACT_ADDRESS,
+                    name: "GachaDrom",
+                    symbol: "GLDR2",
+                    icon: "https://cryptologos.cc/logos/astar-astr-logo.png?v=026"
+                        }
+                ]                }}
                 switchToActiveChain={true}
                 modalSize={"wide"}
+                hideTestnetFaucet={true}
+                displayBalanceToken={{
+                    80001: TOKEN_CONTRACT_ADDRESS,
+                }}
                 welcomeScreen={{
                     img: {
-                        src: "https://github.com/AstarNetwork/brand-assets/blob/main/Astar%20Identity/logo/symbol/Astar_ring.png",
+                        src: "https://i.pinimg.com/564x/76/4a/a5/764aa554d1dedac86fe2abf5ea94de6f.jpg",
                         width: 150,
-                        height: 150,
-                    },
+                        height: 300
+                    }
                 }}
             />
         </div>
-    )
+    );
 }
