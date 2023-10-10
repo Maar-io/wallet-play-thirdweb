a51e7dbf693064e6b5604d3965c2d1b2518a9b7e
Mar.io <34627453+Maar-io@users.noreply.github.com>
2023-10-10 10:19:46 +0200

    Revert "Merge pull request #1 from codespool/feature/connect-wallet"
    This reverts commit d413a318f18e657ab938c80a174876d55c4e2706, reversing
    changes made to 886e5fe1f7092449a5761e4bb72569eacf5aaa03.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,23 +14,36 @@ import { ACCOUNT_FACTORY_ADDRESS } from "../const/addresses";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-export const activeChain = Mumbai;
+const activeChain = Mumbai;
 const smartWalletOptions = {
   factoryAddress: ACCOUNT_FACTORY_ADDRESS,
   gasless: true,
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThirdwebProvider
-      activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      supportedWallets={[
-        smartWallet(metamaskWallet({ recommended: true }), smartWalletOptions),
-        smartWallet(coinbaseWallet(), smartWalletOptions),
-        smartWallet(walletConnect(), smartWalletOptions),
-        smartWallet(embeddedWallet(), smartWalletOptions),
-      ]}
+
+        return (
+          <ThirdwebProvider
+          activeChain={activeChain}
+          clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+            supportedWallets={[
+              smartWallet(
+                metamaskWallet({ recommended: true }),
+                smartWalletOptions,
+              ),
+              smartWallet(
+                coinbaseWallet(),
+                smartWalletOptions,
+              ),
+              smartWallet(
+                walletConnect(),
+                smartWalletOptions,
+              ),
+              smartWallet(
+                embeddedWallet(),
+                smartWalletOptions,
+              ),
+            ]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
